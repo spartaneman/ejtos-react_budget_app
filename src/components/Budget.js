50a72043c3752b7f112db8d2d8bcab2390425172
spartaneman <spartaneman@gmail.com>
2023-06-07 20:33:56 -0400

    updated budget file

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,20 @@
 
+//useContext is a hook that lets you access the context in 
+//a functional component
+import React, { useContext} from 'react';
+//AppContext is a React Context, which provides a way to pass
+//data through the component tree without having to pass props down manually at every level
+import {AppContext} from '../context/AppContext';
+
+const Budget = () => {
+    const{budget} = useContext(AppContext);
+    return (
+        //to make the £ you hold down the alt key and type 0163
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+        </div>
+    );
+
+};
+
+export default Budget;
