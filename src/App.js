fe365a177deb89344e10d44a68e31d797bb82a40
spartaneman <spartaneman@gmail.com>
2023-06-07 21:53:31 -0400

    finished setup for final project

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import ExpenseTotal from './components/ExpenseTotal';
-
-// Add code to import the other components here under
-
-
+import ExpenseList from './components/ExpenseList';
+//import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
@@ -27,15 +26,21 @@ const App = () => {
 
                         <div className= 'col-sm'>
                             <ExpenseTotal />
-                        </div>      
+                        </div> 
+                    </div>
+                    <h3 className='mt-3'>Allocation</h3>
+                    <div className='row'>
+                        <div className= 'col-sm'>
+                            <ExpenseList />    
+                        </div>  
+                    </div> 
+                    <h3 className='mt-3'>Change allocation</h3>
+                    <div className='row mt-3'>
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>    
+                    </div>    
                        
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
-                </div>
             </div>
         </AppProvider>
     );
