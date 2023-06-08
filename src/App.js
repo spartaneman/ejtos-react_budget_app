141ec624c0aabaac239db5c9ea43ead11620601b
spartaneman <spartaneman@gmail.com>
2023-06-07 21:05:01 -0400

    Updated ExpenseTotal and Remaining

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
 
 // Add code to import the other components here under
 
@@ -15,9 +16,13 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                        <div className='col-sm'>
+                            <Budget />    
+                        </div>      
 
-                        {/* Add Remaining component here under */}        
+                        <div className= 'col-sm'>
+                            <Remaining />   
+                        </div>       
 
                         {/* Add ExpenseTotal component here under */}        
                        
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,20 @@
+import React, {useContext} from 'react';
+import { AppContext} from '../context/AppContext';
 
+const Remaining = ()=>{
+    const {expenses, budget} = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item)=> {
+        //total is the running tally, while item is the item 
+        //that the array is currently on. 
+        //reduce(adds the value of an array into a scalar)
+        return (total = total + item.cost);
+    }, 0);
+    const alertType = totalExpenses > budget ? 'alert-danger': 'alert-success';
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: £{budget - totalExpenses}</span>
+        </div>
+    );
+};
+
+export default Remaining;
