b362c03929f3e1e99bbad30ce9be2126c3131401
spartaneman <spartaneman@gmail.com>
2023-06-08 15:18:18 -0400

    Completed task 1 and 2

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -6,12 +6,43 @@ import React, { useContext} from 'react';
 //data through the component tree without having to pass props down manually at every level
 import {AppContext} from '../context/AppContext';
 
+
+
+
 const Budget = () => {
-    const{ budget } = useContext(AppContext);
+
+    const{ budget, dispatch, expenses } = useContext(AppContext);
+    
+    const totalExpenses = expenses.reduce((total, item)=>{
+        return (total = total + item.cost);
+    },0)
+    const budgetChangeHandler = (event) =>{
+        console.log(event.target.value);
+        console.log(totalExpenses);
+        if( event.target.value > 20000){
+            alert("Sorry Budget has A max value of 20,000")
+            return;
+        }
+
+        if(event.target.value < totalExpenses){
+            alert("Sorry the budget cannot be lower than the current total expenses");
+            return;
+        }
+
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: parseInt(event.target.value, 10)
+        });
+        
+
+    }
     return (
         //to make the £ you hold down the alt key and type 0163
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £</span>
+            <input id='budget' type='number' step={10} value={budget} onChange={budgetChangeHandler}>
+                
+            </input>
         </div>
     );
 
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -60,7 +60,7 @@ export const AppReducer = (state, action) => {
         case 'SET_BUDGET':
             action.type = "DONE";
             state.budget = action.payload;
-
+            console.log(state.budget)
             return {
                 ...state,
             };
